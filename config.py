29954f7f97af6feae654f7cd870ed919f3996c87
Nice0Man <e.krotov03@gmail.com>
2022-04-06 22:20:46 +0300

    deleted russian comments

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ import my_math
 import cmath
 import fractions
 
-# Это обязательно удали!
-# Все что будет написано в коментарриях на английском оставь. Инфа на русском чисто для тебя.
-# Чтобы лучше понять, что тут происходит почитай о словарях, множествах и о функции eval()
-# Так же я написал небольшую билиотеку с функциями, которых нет в дефолтной "math" и которые нужны тебе по ТЗ
-
 math_dict = {
     name: exp for name, exp in math.__dict__.items() if not name.startswith("__")
 }  # это словарь
@@ -23,10 +18,6 @@ fractions_math = {
 my_math_dict = {
     name: exp for name, exp in my_math.__dict__.items() if not name.startswith("__") if not name.startswith("common")
 }
-
-# В данном случае "name" это диначический ключ, формируемый из функций доступных в библиотеке "math",
-# а все что после ":" это соответсвенные значения ключей
-# вроде как это назыается мэппинг
 
 math_dict.update(my_math_dict)
 math_dict.update(c_math)
@@ -48,4 +39,3 @@ You can use any of the following functions and constants:
 
 {', '.join(ALLOWED_NAMES.keys())}
 """
-# а это множество
--- main.py
+++ main.py
@@ -3,28 +3,20 @@ import config as cfg
 
 def calculate(expression):
     code = compile(expression, "<string>", "eval")
-    # компилируем введенную строку в код
     # compiling in byte-code
     for name in code.co_names:
-        # проходим по списку математических действий
         if name not in cfg.ALLOWED_NAMES:
-            # если действия нет, то возвращаем NameError, в котором говорим,
-            # что данного действия нет в списке доступных
             raise NameError(f"The use of '{name}' is not allowed")
     return eval(code, {"__builtins__": {}}, cfg.ALLOWED_NAMES)
-    # Если все окей, возвращем результат выполнения функции
 
 
 def main():
     print(cfg.WELCOME_MESSAGE)
     while True:
-        # Запускаем бесконечный цикл
         # Reading user input
         try:
             expression = input(f"{cfg.ARROW} ")
         except (KeyboardInterrupt, EOFError):
-            # В этом блоке мы проверяем правильность данных и не прерван ли поток
-            # ввода, если прерван и вылезают эти ошибки, мы завершаем выполнение программы
             raise SystemExit()
 
         # Custom commands support
@@ -38,13 +30,10 @@ def main():
         try:
             result = calculate(expression)
         except SyntaxError:
-            # Если юзер не умеет писать, обрабатываем SyntaxError
             # Incorrect expression
             print("You entered an incorrect expression.")
             continue
         except (NameError, ValueError) as err:
-            # Если юзер умеет писать, но не умеет думать (99.9% что это так)
-            # обрабатываем NameError и ValueError
             # If the user tried to use an unauthorized name
             # or an invalid value in a passed function
             print(err)
